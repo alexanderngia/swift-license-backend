b325036204c1a3aaaef80258d3e6e894914cecaf
Alexander <alexanderngia@gmail.com>
2024-03-15 23:26:48 +0700

    update 23:26 15/3/2024

--- src/config/connectDB.js
+++ src/config/connectDB.js
@@ -1,5 +1,7 @@
 const { Sequelize } = require("sequelize");
 
+require("dotenv").config();
+
 // // Option 1: Passing a connection URI
 // const sequelize = new Sequelize('sqlite::memory:') // Example for sqlite
 // const sequelize = new Sequelize('postgres://user:pass@example.com:5432/dbname') // Example for postgres
@@ -11,11 +13,22 @@ const { Sequelize } = require("sequelize");
 // });
 
 // Option 3: Passing parameters separately (other dialects)
-const sequelize = new Sequelize("swift_manager", "root", null, {
-  host: "127.0.0.1",
-  dialect: "mysql",
-  logging: false,
-});
+// const sequelize = new Sequelize("swift_manager", "root", null, {
+//   host: "127.0.0.1",
+//   dialect: "mysql",
+//   logging: false,
+// });
+
+const sequelize = new Sequelize(
+  process.env.DB_DATABASE,
+  process.env.DB_USERNAME,
+  `${process.env.DB_PASSWORD}`,
+  {
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_DIALECT,
+    logging: false,
+  }
+);
 
 let connectDB = async () => {
   try {
