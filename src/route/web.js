4064537c11ba0ebd6cf256682c9186edb780df5b
Alexander <alexanderngia@gmail.com>
2024-03-20 15:13:40 +0700

    update 15:13 20/3/24

--- src/route/web.js
+++ src/route/web.js
@@ -2,11 +2,24 @@ import express from "express";
 // import userController from "../controllers/userController";
 import { homeController } from "../controllers/homeController.js";
 import { licenseController } from "../controllers/licenseController.js";
+import cors from "cors";
+import "dotenv/config";
 
 let router = express.Router();
 
+var allowlist = [`${process.env.DOMAIN_CLIENT}`];
+var corsOptionsDelegate = function (req, callback) {
+  var corsOptions;
+  if (allowlist.indexOf(req.header("Origin")) !== -1) {
+    corsOptions = { origin: true }; // reflect (enable) the requested origin in the CORS response
+  } else {
+    corsOptions = { origin: false }; // disable CORS for this request
+  }
+  callback(null, corsOptions); // callback expects two parameters: error and options
+};
+
 export const initWebRoutes = (app) => {
-  router.get("/", homeController.getHomePage);
+  router.get("/", cors(corsOptionsDelegate), homeController.getHomePage);
   // // API AUTHENTICATE
   // router.post("/api/auth/signin", userController.handleLogin);
   // router.post("/api/auth/signup", userController.handleRegister);
@@ -17,15 +30,29 @@ export const initWebRoutes = (app) => {
   // router.delete("/api/user/delete", userController.handleDeleteUser);
 
   // API LICENSE KEY
-  router.get("/api/license-key", licenseController.handleGetAllLicense);
-  router.post("/api/license-key/create", licenseController.handleCreateLicense);
+  router.get(
+    "/api/license-key",
+    cors(corsOptionsDelegate),
+    licenseController.handleGetAllLicense
+  );
+  router.post(
+    "/api/license-key/create",
+    cors(corsOptionsDelegate),
+    licenseController.handleCreateLicense
+  );
   router.post(
     "/api/license-key/authenticate",
+    cors(corsOptionsDelegate),
     licenseController.handleAuthenticateLicense
   );
-  router.put("/api/license-key/edit", licenseController.handleEditLicense);
+  router.put(
+    "/api/license-key/edit",
+    cors(corsOptionsDelegate),
+    licenseController.handleEditLicense
+  );
   router.delete(
     "/api/license-key/delete",
+    cors(corsOptionsDelegate),
     licenseController.handleDeleteLicense
   );
 
